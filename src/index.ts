724d5282776fbfcb52732091420f1829d53e27f1
rozita-hasani <rozita.hasani.work@gmail.com>
2026-07-22 14:35:47 +0200

    feat: install jwt

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,10 @@ const app = express();
 app.use(express.json());
 app.use("/auth", authRoutes);
 
+app.get("/", (req, res) => {
+    res.json({
+        message: "SmartBudget API is running"
+    });
+});
+
 app.listen(3000);
