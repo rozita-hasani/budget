2674f567461c1e3667938616b3ac019be5354c84
rozita-hasani <rozita.hasani.work@gmail.com>
2026-07-22 15:12:41 +0200

    feat: create login endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import bcrypt from "bcrypt";
 import { db } from "../db";
 import { users } from "../db/schema";
+import {eq} from "drizzle-orm";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
@@ -20,5 +22,40 @@ router.post("/register", async (req, res) => {
 
     res.status(201).json(createdUser[0]);
 });
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await db.query.users.findFirst({
+        where: eq(users.email, email)
+    })
+
+    if (!user) {
+        return res.status(401).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const isPassValid = await bcrypt.compare(password, user.passwordHash);
+
+    if (!isPassValid) {
+        return res.status(401).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const token = jwt.sign(
+        {
+            userId: user.id,
+            email: user.email,
+        },
+        process.env.JWT_SECRET!,
+        {
+            expiresIn: "24h"
+        }
+    )
+
+    return res.status(200).json({token})
+})
 
 export default router;
