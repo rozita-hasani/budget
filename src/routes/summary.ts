b52d21b0c84e0dedf5b270fef9a426f76f58ccc6
rozita-hasani <rozita.hasani.work@gmail.com>
2026-07-29 09:23:48 +0200

    feat: add month year filtering to summary endpoints

--- src/routes/summary.ts
+++ src/routes/summary.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import {db} from "../db";
 import {categories, transactions} from "../db/schema";
-import {and, eq, sum} from "drizzle-orm";
+import {and, eq, sum, gte, lt} from "drizzle-orm";
 
 const router = express.Router();
 
 router.get("/monthly", async (req, res) => {
     const userId = req.user!.userId;
+    const year = Number(req.query.year);
+    const month = Number(req.query.month);
+    const startDate = new Date(year, month - 1, 1);
+    const endDate = new Date(year, month, 1);
 
     const incomeResult = await db
         .select({
@@ -16,7 +20,9 @@ router.get("/monthly", async (req, res) => {
         .where(
             and(
                 eq(transactions.userId, userId),
-                eq(transactions.type, "INCOME")
+                eq(transactions.type, "INCOME"),
+                gte(transactions.date, startDate),
+                lt(transactions.date, endDate),
             )
         );
     const income = Number(incomeResult[0].total ?? 0);
@@ -29,7 +35,9 @@ router.get("/monthly", async (req, res) => {
         .where(
             and(
                 eq(transactions.userId, userId),
-                eq(transactions.type, "EXPENSE")
+                eq(transactions.type, "EXPENSE"),
+                gte(transactions.date, startDate),
+                lt(transactions.date, endDate),
             )
         );
     const expense = Number(expenseResult[0].total ?? 0);
@@ -41,6 +49,10 @@ router.get("/monthly", async (req, res) => {
 
 router.get("/by-category", async (req, res) => {
     const userId = req.user!.userId;
+    const year = Number(req.query.year);
+    const month = Number(req.query.month);
+    const startDate = new Date(year, month - 1, 1);
+    const endDate = new Date(year, month, 1);
 
     const result  = await db
         .select({
@@ -55,7 +67,9 @@ router.get("/by-category", async (req, res) => {
         .where(
             and(
                 eq(transactions.userId, userId),
-                eq(transactions.type, "EXPENSE")
+                eq(transactions.type, "EXPENSE"),
+                gte(transactions.date, startDate),
+                lt(transactions.date, endDate),
             )
         )
         .groupBy(categories.name);
